4dfb2d9660854d2620b3ea60d93c9f4716b0a2f1
Oğuzhan Öncü <oguzhan.oncu7@gmail.com>
2023-06-08 10:15:09 +0200

    Page proposition affiche le film clické

--- app.js
+++ app.js
@@ -3,45 +3,47 @@
  **********/
 
 // code pour afficher et enlever le loader
-const loading = document.getElementById("loader");
-const content = document.getElementById("content");
 
-document.onload = setTimeout(showContent, 3500);
+if (window.location.pathname == '/movie-quiz.html') {
+    const loading = document.getElementById("loader");
+    const content = document.getElementById("content");
 
-function showContent() {
-    loading.style.display = "none";
-    content.style.display = "block";
-}
+    document.onload = setTimeout(showContent, 3500);
 
-
-let titreLoader = document.getElementById("titre-loader");
-let spans = document.querySelectorAll("h1 span");
-let title = titreLoader.innerText;
-
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z']
-const titre = ["M", "O", "V", "I", "E", " ", "Q", "U", "I", "Z"];
-
-
-const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
-
-const defilement = async () => {
-    let index = 0;
-    for (lettre of spans) { /* pour chaque lettre du titre */
-        for (const item of alphabet) { /* pour chaque lettre de l'alphabet */
-            await sleep(6) /* attend entre chaque lettre de l'alphabet */
-            lettre.innerHTML = item;
-        }
-        lettre.innerHTML = titre[index];
-        index++
+    function showContent() {
+        loading.style.display = "none";
+        content.style.display = "block";
     }
+
+
+    let titreLoader = document.getElementById("titre-loader");
+    let spans = document.querySelectorAll("h1 span");
+    let title = titreLoader.innerText;
+
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z']
+    const titre = ["M", "O", "V", "I", "E", " ", "Q", "U", "I", "Z"];
+
+
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    const defilement = async () => {
+        let index = 0;
+        for (lettre of spans) { /* pour chaque lettre du titre */
+            for (const item of alphabet) { /* pour chaque lettre de l'alphabet */
+                await sleep(6) /* attend entre chaque lettre de l'alphabet */
+                lettre.innerHTML = item;
+            }
+            lettre.innerHTML = titre[index];
+            index++
+        }
+    }
+
+    defilement()
 }
-
-defilement()
-
 
 /**********
  * SWIPER *
@@ -53,3 +55,26 @@ var swiper = new Swiper(".myswiper", {
     freeMode: true,
 
 });
+
+
+/********************
+ * PAGE PROPOSITION *
+ ********************/
+
+
+contentQuiz = document.getElementById("content-quiz");
+const buttons = document.querySelectorAll(".swiper-slide img");
+const buttonPressed = e => {
+    localStorage.setItem('lien', e.target.attributes['src'].value);
+}
+
+for (let button of buttons) {
+    button.addEventListener("click", buttonPressed);
+}
+
+
+let lien = localStorage.getItem('lien');
+if (window.location.pathname == '/quiz.html') {
+
+    contentQuiz.style.backgroundImage = "url('" + lien + "')";
+}
