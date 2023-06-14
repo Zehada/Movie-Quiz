6e0586e8183199cbe4a7f5d7bc3e0bf7ddd3a2be
Oğuzhan Öncü <oguzhan.oncu7@gmail.com>
2023-06-14 15:35:37 +0200

    Page d'info des films trouvés fonctionne avec API

--- app.js
+++ app.js
@@ -89,22 +89,7 @@ if (window.location.pathname == '/quiz.html') {
  * JSON *
  ********/
 
-// fetch('/data.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
 
-
-
-// var obj;
-
-// fetch('data.json')
-//     .then(res => res.json())
-//     .then(data => {
-//         obj = data;
-//     })
-//     .then(() => {
-//         console.log(obj.movies.movie[0]);
-//     });
 
 fetch('data.json')
     .then(jsonData => jsonData.json())
@@ -113,21 +98,36 @@ fetch('data.json')
 let printIt = (data) => {
     if (window.location.pathname == '/quiz.html') {
         sessionStorage.viewed = 1;
+        var input = document.getElementById("fname");
+        input.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                document.getElementById("soumettre").click();
+            }
+        });
         document.getElementById("soumettre").addEventListener("click", function () {
             for (i = 0; i < Object.keys(data.movies.movie).length; i++) {
-                if ((document.querySelector("input").value === data.movies.movie[i].title) && (lien === data.movies.movie[i].picture)) {
+                if ((document.querySelector("input").value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") === data.movies.movie[i].title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) && (lien === data.movies.movie[i].picture)) {
                     localStorage.setItem(("trouvé" + i), data.movies.movie[i].found)
                     document.getElementById("bonne-reponse").style.display = "block";
-                    window.location.replace("movie-quiz.html");
+                    document.getElementById("mauvaise-reponse").style.display = "none";
+                    setTimeout(function () { window.location.replace("movie-quiz.html") }, 3000);
 
+                } else if ((document.querySelector("input").value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") != data.movies.movie[i].title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) && (lien === data.movies.movie[i].picture)) {
+                    document.querySelector("input").value = "";
+                    document.getElementById("mauvaise-reponse").style.display = "block";
                 }
             }
             for (i = 0; i < Object.keys(data.movies.serie).length; i++) {
-                if ((document.querySelector("input").value === data.movies.serie[i].title) && (lien === data.movies.serie[i].picture)) {
+                if ((document.querySelector("input").value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") === data.movies.serie[i].title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) && (lien === data.movies.serie[i].picture)) {
                     localStorage.setItem(("trouvée" + i), data.movies.serie[i].found)
                     document.getElementById("bonne-reponse").style.display = "block";
-                    window.location.replace("movie-quiz.html");
+                    document.getElementById("mauvaise-reponse").style.display = "none";
+                    setTimeout(function () { window.location.replace("movie-quiz.html") }, 3000);
 
+                } else if ((document.querySelector("input").value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") != data.movies.serie[i].title.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")) && (lien === data.movies.serie[i].picture)) {
+                    document.querySelector("input").value = "";
+                    document.getElementById("mauvaise-reponse").style.display = "block";
                 }
             }
 
@@ -137,34 +137,108 @@ let printIt = (data) => {
 
 
     }
-    let filmATrouver = document.querySelectorAll(".filmatrouver");
-    for (i = 0; i < data.movies.movie.length; i++) {
-        if (localStorage.getItem("trouvé" + i)) {
-            document.getElementById("filmstrouves").innerHTML += "<div class='swiper-slide trouve'><img src='" + localStorage.getItem("trouvé" + i) + "'></div>";
-            for (div of filmATrouver) {
-                if (div.querySelector("img").attributes['src'].value === data.movies.movie[i].picture) {
-                    div.remove();
+    if (window.location.pathname == '/movie-quiz.html') {
+        let filmATrouver = document.querySelectorAll(".filmatrouver");
+        for (i = 0; i < data.movies.movie.length; i++) {
+            if (localStorage.getItem("trouvé" + i)) {
+                document.getElementById("filmstrouves").innerHTML += "<div class='swiper-slide trouve'><a href='quiz2.html' target='_blank'><img class='" + data.movies.movie[i].id + "' src='" + localStorage.getItem("trouvé" + i) + "'></a></div>";
+                for (div of filmATrouver) {
+                    if (div.querySelector("img").attributes['src'].value === data.movies.movie[i].picture) {
+                        div.remove();
+
+                    }
 
                 }
-
             }
+
+        }
+
+        let serieATrouver = document.querySelectorAll(".serieatrouver");
+        for (i = 0; i < data.movies.serie.length; i++) {
+            if (localStorage.getItem("trouvée" + i)) {
+                document.getElementById("seriestrouvees").innerHTML += "<div class='swiper-slide trouve'><a href='quiz2.html' target='_blank'><img class='" + data.movies.serie[i].id + "' src='" + localStorage.getItem("trouvée" + i) + "'></a></div>";
+                for (divs of serieATrouver) {
+                    if (divs.querySelector("img").attributes['src'].value === data.movies.serie[i].picture) {
+                        divs.remove();
+
+                    }
+
+                }
+            }
+
+        }
+    }
+
+
+    const contentQuizDeux = document.getElementById("content-quiz2");
+    const imageTrouvee = document.querySelectorAll(".trouve img");
+
+    const ImagePressed = e => {
+        localStorage.setItem('ImageTrouvee', e.target.attributes['src'].value);
+        localStorage.setItem('idtrouve', e.target.className);
+    }
+
+    for (let image of imageTrouvee) {
+        image.addEventListener("click", ImagePressed);
+        console.log(image)
+    }
+
+
+    let TrouveeImage = localStorage.getItem('ImageTrouvee');
+    if (window.location.pathname == '/quiz2.html') {
+        // contentQuizDeux.style.backgroundImage = "url('" + TrouveeImage + "')";
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNTJjNzZiNGY4MTI0MGE1ZDliNmVhNDI1YjI1ZTYzZiIsInN1YiI6IjY0ODZmYmM4OTkyNTljMDBhY2NkY2Q1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yM3TXK2nsVecbZFkVPLjsLuS3loYN_zw1q92CQXVT8M'
+            }
+        };
+
+        fetch('https://api.themoviedb.org/3/movie/' + localStorage.getItem("idtrouve") + '?language=fr-FR', options)
+            .then(response => response.json())
+            .then(data => printu(data))
+
+            .catch(err => console.error(err));
+
+        let printu = (data) => {
+            document.getElementById("movietitle").innerHTML = data.title;
+            document.getElementById("synopsis").innerHTML = data.overview;
+        }
+
+        fetch('https://api.themoviedb.org/3/movie/' + localStorage.getItem("idtrouve") + '/images', options)
+            .then(response => response.json())
+            .then(data => printimage(data))
+
+            .catch(err => console.error(err));
+
+        let printimage = (data) => {
+            contentQuizDeux.style.backgroundImage = "url('https://image.tmdb.org/t/p/original" + data.backdrops[0].file_path + "')";
+        }
+
+
+        fetch('https://api.themoviedb.org/3/movie/' + localStorage.getItem("idtrouve") + '/credits?language=fr-FR', options)
+            .then(response => response.json())
+            .then(data => printActor(data))
+
+            .catch(err => console.error(err));
+
+        let printActor = (data) => {
+            for (actor of data.cast)
+                if (actor.profile_path) {
+                    document.querySelector(".acteurs").innerHTML += "<div class='swiper-slide'><img src='https://image.tmdb.org/t/p/original" + actor.profile_path + "' alt='" + actor.name + "'><h4 class='mt-4'>" + actor.name + "</h4><h5>" + actor.character + "</h5></div>"
+                }
+
         }
 
     }
 
-    let serieATrouver = document.querySelectorAll(".serieatrouver");
-    for (i = 0; i < data.movies.serie.length; i++) {
-        if (localStorage.getItem("trouvée" + i)) {
-            document.getElementById("seriestrouvees").innerHTML += "<div class='swiper-slide trouve'><img src='" + localStorage.getItem("trouvée" + i) + "'></div>";
-            for (divs of serieATrouver) {
-                if (divs.querySelector("img").attributes['src'].value === data.movies.serie[i].picture) {
-                    divs.remove();
 
-                }
 
-            }
-        }
-
-    }
 }
+
+
+
+
+
 
